7c316bcbb005d5f9de5949a5b6c3420a5164cb02
Seon-Wook Park <seon.wook@swook.net>
2011-10-31 16:07:49 +0800

    Made script Gnome, etc compatible

---
+++ update-synclient-settings.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+#   Synclient settings saver
+#
+# - Author: Seon Wook Park
+# - Date: 31st Oct 2011
+#
+# Run after changing settings via `synclient`
+
+APPNAME="apply-synclient-settings"
+TMPFILE=".synclient.out"
+
+# Check if synclient is accessible
+if [[ `command -v synclient` == '' ]]; then
+	echo "Error: synclient is not installed"
+	exit
+fi
+
+if [[ $DESKTOP_SESSION == "akde-plasma" ]]; then
+	TMPFOLDER="/tmp/kde-"$USER
+else
+	TMPFOLDER="/tmp"
+fi
+SYNFILE=$HOME"/.config/"$APPNAME".sh"
+
+# Change directory to tmp
+cd $TMPFOLDER
+
+# Remove synclient updating file if it exists
+if [ -e $SYNFILE ]; then
+	rm $SYNFILE
+fi
+
+# Add bash header to new file
+echo "#!/bin/bash" >> $SYNFILE
+
+# Grab output from synclient
+echo "Grabbing current settings from synclient..."
+synclient -l > $TMPFILE
+
+# Parse synclient output
+echo "Parsing settings..."
+while read line; do
+	if [[ $line =~ ([[:alpha:]]+)[[:space:]]*=[[:space:]]*([0-9\.]+) ]]; then
+		echo "synclient "${BASH_REMATCH[1]}"="${BASH_REMATCH[2]} >> $SYNFILE
+		echo "	"${BASH_REMATCH[1]}"="${BASH_REMATCH[2]}
+	fi
+done < $TMPFILE
+rm $TMPFILE
+
+# Make output file executable
+chmod +x $SYNFILE
+
+echo; echo "Settings script created in "$SYNFILE
+
+
+# Add .desktop for non-KDE DEs
+DPFILE=$HOME"/.config/autostart/"$APPNAME".desktop"
+
+if [ -e $DPFILE ]; then
+	rm $DPFILE
+fi
+
+echo "[Desktop Entry]" >> $DPFILE
+echo "Type=Application" >> $DPFILE
+echo "Exec="$SYNFILE >> $DPFILE
+echo "Hidden=false" >> $DPFILE
+echo "NoDisplay=false" >> $DPFILE
+echo "Name=Apply Synclient Settings" >> $DPFILE
+echo "Comment=Apply your saved synclient settings" >> $DPFILE
+echo "X-GNOME-Autostart-enabled=true" >> $DPFILE
+
+echo "Added script to startup"
